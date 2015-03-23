28fe7a2a70e80da350c295560ffa596f65b0a604
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-03-23 17:17:19 +0200

    1. Try to use CytoscapeJS (http://js.cytoscape.org/)

--- web/js/ng-app.js
+++ web/js/ng-app.js
@@ -1,18 +1,91 @@
-(function (angular, undercore) {
+(function (angular, undercore, cy) {
   angular.module('underscore', [])
     .factory('_', function () {
       return undercore;
     });
 
-  angular.module('ng-app', ['underscore', 'angular-growl'])
+  angular.module('cytoscape', [])
+    .factory('cy', function () {
+      return cytoscape
+    });
+
+  angular.module('ng-app', ['underscore', 'angular-growl', 'cytoscape'])
     .config(['growlProvider', function (growlProvider) {
       growlProvider.globalReversedOrder(true);
       growlProvider.globalTimeToLive(3000);
     }])
-    .controller('MainController', function ($scope, $http, growl, _) {
+    .controller('MainController', function ($scope, $http, growl, _, cy) {
       $scope.questionList = undefined;
       $scope.currentQuestion = undefined;
       $scope.answers = [];
+
+      $scope.$watch('currentQuestion', function () {
+        var cyObject = cy({
+          container: document.getElementById('cy-container'),
+          style: cy.stylesheet()
+            .selector('node')
+            .css({
+              'content': 'data(id)'
+            })
+            .selector('edge')
+            .css({
+              'target-arrow-shape': 'triangle',
+              'width': 4,
+              'line-color': '#ddd',
+              'target-arrow-color': '#ddd'
+            })
+            .selector('.highlighted')
+            .css({
+              'background-color': '#61bffc',
+              'line-color': '#61bffc',
+              'target-arrow-color': '#61bffc',
+              'transition-property': 'background-color, line-color, target-arrow-color',
+              'transition-duration': '0.5s'
+            }),
+          layout: {
+            name: 'breadthfirst',
+            directed: true,
+            roots: '#a',
+            padding: 10
+          },
+          elements: {
+            nodes: [
+              {data: {id: 'a'}},
+              {data: {id: 'b'}},
+              {data: {id: 'c'}},
+              {data: {id: 'd'}},
+              {data: {id: 'e'}}
+            ],
+
+            edges: [
+              {data: {id: 'a"e', weight: 1, source: 'a', target: 'e'}},
+              {data: {id: 'ab', weight: 3, source: 'a', target: 'b'}},
+              {data: {id: 'be', weight: 4, source: 'b', target: 'e'}},
+              {data: {id: 'bc', weight: 5, source: 'b', target: 'c'}},
+              {data: {id: 'ce', weight: 6, source: 'c', target: 'e'}},
+              {data: {id: 'cd', weight: 2, source: 'c', target: 'd'}},
+              {data: {id: 'de', weight: 7, source: 'd', target: 'e'}}
+            ]
+          }
+        });
+
+        var bfs = cyObject.elements().bfs('#a', function () {
+
+        }, true);
+
+        var i = 0;
+        var highlightNextEle = function(){
+          bfs.path[i].addClass('highlighted');
+
+          if( i < bfs.path.length ){
+            i++;
+            setTimeout(highlightNextEle, 1000);
+          }
+        };
+
+// kick off first highlight
+        highlightNextEle();
+      });
 
       var results;
 
@@ -105,4 +178,5 @@
         };
       }
     });
-})(window.angular, window._);
+})
+(window.angular, window._, window.cy);
