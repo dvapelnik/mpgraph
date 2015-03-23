e1950a060acec393d53dd66ab27b215c9411059c
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-03-23 17:37:09 +0200

    1. Question graph changed

--- web/js/questions.js
+++ web/js/questions.js
@@ -1,340 +1,69 @@
 var data = {
   "questions": [
     {
-      "id": 0,
-      "question": "?",
-      "answers": [
-        {
-          "id": 0,
-          "answer": "+",
-          "isPositive": true,
-          "nextQuestionId": 1
-        },
-        {
-          "id": 1,
-          "answer": "-",
-          "isPositive": false,
-          "nextQuestionId": 2
-        }
-      ]
+      "id": 1, "question": "?", "answers": [
+      {"id": 1, "answer": "+", "isPositive": true, "nextQuestionId": 2},
+      {"id": 2, "answer": "-", "isPositive": false, "nextQuestionId": 3}
+    ]
     },
     {
-      "id": 1,
-      "question": "+?",
-      "answers": [
-        {
-          "id": 2,
-          "answer": "++",
-          "isPositive": true,
-          "nextQuestionId": 3
-        },
-        {
-          "id": 3,
-          "answer": "+-",
-          "isPositive": false,
-          "nextQuestionId": 4
-        }
-      ]
+      "id": 2, "question": "?", "answers": [
+      {"id": 3, "answer": "+", "isPositive": true, "nextQuestionId": 4},
+      {"id": 4, "answer": "-", "isPositive": false, "nextQuestionId": 5}
+    ]
     },
     {
-      "id": 2,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 4,
-          "answer": "+-",
-          "isPositive": true,
-          "nextQuestionId": 5
-        },
-        {
-          "id": 5,
-          "answer": "--",
-          "isPositive": false,
-          "nextQuestionId": 6
-        }
-      ]
+      "id": 3, "question": "?", "answers": [
+      {"id": 5, "answer": "+", "isPositive": true, "nextQuestionId": 5},
+      {"id": 6, "answer": "-", "isPositive": false, "nextQuestionId": 6}
+    ]
     },
     {
-      "id": 3,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 6,
-          "answer": "+-",
-          "isPositive": true,
-          "nextQuestionId": 7
-        },
-        {
-          "id": 7,
-          "answer": "--",
-          "isPositive": false,
-          "nextQuestionId": 8
-        }
-      ]
+      "id": 4, "question": "?", "answers": [
+      {"id": 20, "answer": "+", "isPositive": true, "nextQuestionId": 9},
+      {"id": 7, "answer": "-", "isPositive": false, "nextQuestionId": 7}
+    ]
     },
     {
-      "id": 4,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 8,
-          "answer": "+-",
-          "isPositive": true,
-          "nextQuestionId": 9
-        },
-        {
-          "id": 9,
-          "answer": "--",
-          "isPositive": false,
-          "nextQuestionId": 10
-        }
-      ]
+      "id": 5, "question": "?", "answers": [
+      {"id": 8, "answer": "+", "isPositive": true, "nextQuestionId": 7},
+      {"id": 9, "answer": "-", "isPositive": false, "nextQuestionId": 8}
+    ]
     },
     {
-      "id": 5,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 10,
-          "answer": "+-",
-          "isPositive": true,
-          "nextQuestionId": 11
-        },
-        {
-          "id": 11,
-          "answer": "--",
-          "isPositive": false,
-          "nextQuestionId": 12
-        }
-      ]
+      "id": 6, "question": "?", "answers": [
+      {"id": 10, "answer": "+", "isPositive": true, "nextQuestionId": 8},
+      {"id": 15, "answer": "-", "isPositive": false, "nextQuestionId": 10}
+    ]
     },
     {
-      "id": 6,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 12,
-          "answer": "+-",
-          "isPositive": true,
-          "nextQuestionId": 13
-        },
-        {
-          "id": 13,
-          "answer": "--",
-          "isPositive": false,
-          "nextQuestionId": 14
-        }
-      ]
+      "id": 7, "question": "?", "answers": [
+      {"id": 11, "answer": "+", "isPositive": true, "nextQuestionId": 9},
+      {"id": 12, "answer": "-", "isPositive": false, "nextQuestionId": 10}
+    ]
     },
     {
-      "id": 7,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 14,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 0
-        },
-        {
-          "id": 15,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 1
-        }
-      ]
+      "id": 8, "question": "?", "answers": [
+      {"id": 13, "answer": "+", "isPositive": true, "nextQuestionId": 7},
+      {"id": 14, "answer": "-", "isPositive": false, "nextQuestionId": 10}
+    ]
     },
     {
-      "id": 8,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 16,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 2
-        },
-        {
-          "id": 17,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 3
-        }
-      ]
+      "id": 9, "question": "?", "answers": [
+      {"id": 16, "answer": "+", "isPositive": true, "resultId": 1},
+      {"id": 17, "answer": "-", "isPositive": false, "resultId": 2}
+    ]
     },
     {
-      "id": 9,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 18,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 4
-        },
-        {
-          "id": 19,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 5
-        }
-      ]
-    },
-    {
-      "id": 10,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 20,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 6
-        },
-        {
-          "id": 21,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 7
-        }
-      ]
-    },
-    {
-      "id": 11,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 22,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 8
-        },
-        {
-          "id": 23,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 9
-        }
-      ]
-    },
-    {
-      "id": 12,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 24,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 10
-        },
-        {
-          "id": 25,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 11
-        }
-      ]
-    },
-    {
-      "id": 13,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 26,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 12
-        },
-        {
-          "id": 27,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 13
-        }
-      ]
-    },
-    {
-      "id": 14,
-      "question": "-?",
-      "answers": [
-        {
-          "id": 28,
-          "answer": "+-",
-          "isPositive": true,
-          "resultId": 14
-        },
-        {
-          "id": 29,
-          "answer": "--",
-          "isPositive": false,
-          "resultId": 15
-        }
-      ]
+      "id": 10, "question": "?", "answers": [
+      {"id": 18, "answer": "+", "isPositive": true, "resultId": 2},
+      {"id": 19, "answer": "-", "isPositive": false, "resultId": 3}
+    ]
     }
   ],
   "results": [
-    {
-      "id": 0,
-      "result": "Result 0"
-    },
-    {
-      "id": 1,
-      "result": "Result 1"
-    },
-    {
-      "id": 2,
-      "result": "Result 2"
-    },
-    {
-      "id": 3,
-      "result": "Result 3"
-    },
-    {
-      "id": 4,
-      "result": "Result 4"
-    },
-    {
-      "id": 5,
-      "result": "Result 5"
-    },
-    {
-      "id": 6,
-      "result": "Result 6"
-    },
-    {
-      "id": 7,
-      "result": "Result 7"
-    },
-    {
-      "id": 8,
-      "result": "Result 8"
-    },
-    {
-      "id": 9,
-      "result": "Result 9"
-    },
-    {
-      "id": 10,
-      "result": "Result 10"
-    },
-    {
-      "id": 11,
-      "result": "Result 11"
-    },
-    {
-      "id": 12,
-      "result": "Result 12"
-    },
-    {
-      "id": 13,
-      "result": "Result 13"
-    },
-    {
-      "id": 14,
-      "result": "Result 14"
-    },
-    {
-      "id": 15,
-      "result": "Result 15"
-    }
-  ]
+    {"id": 1, "result": "Result 1"},
+    {"id": 2, "result": "Result 2"},
+    {"id": 3, "result": "Result 3"}
+  ],
 };
